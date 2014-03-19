9a404d4ed55db709d0d46460a91b13f3688a23fa
semmara <semmara@users.noreply.github.com>
2014-03-20 00:03:03 +0100

    no need for select

--- popen_realtime_output.py
+++ popen_realtime_output.py
@@ -4,7 +4,7 @@
 
 import subprocess as sub
 import sys
-import select
+#import select
 import shlex
 
 cmd = shlex.split("nc -l localhost 9966")
@@ -13,7 +13,7 @@ if len(sys.argv) > 1:
 p = sub.Popen(cmd, stdout=sub.PIPE)
 
 while True:
-	select.select([p.stdout.fileno()], [], [])
+	#select.select([p.stdout.fileno()], [], [])
 	ch = p.stdout.read(1)
 	if len(ch) == 0:
 		break
