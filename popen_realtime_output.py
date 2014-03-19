ebd1e545667fadcc9f98044f41fed5dc3c0166d0
semmara <semmara@users.noreply.github.com>
2014-03-19 21:29:01 +0100

    updated argv handling

--- popen_realtime_output.py
+++ popen_realtime_output.py
@@ -5,8 +5,12 @@
 import subprocess as sub
 import sys
 import select
+import shlex
 
-p = sub.Popen(sys.argv[1:], stdout=sub.PIPE)
+cmd = shlex.split("nc -l localhost 9966")
+if len(sys.argv) > 1:
+	cmd = sys.argv[1:]
+p = sub.Popen(cmd, stdout=sub.PIPE)
 
 while True:
 	select.select([p.stdout.fileno()], [], [])
