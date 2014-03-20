81fc55fe46046e7909dfb6b1f90cde19eb0083f5
semmara <semmara@users.noreply.github.com>
2014-03-20 09:19:57 +0100

    extended to work with stdout and stderr

--- popen_realtime_output.py
+++ popen_realtime_output.py
@@ -4,19 +4,30 @@
 
 import subprocess as sub
 import sys
-#import select
+import select
 import shlex
 
 cmd = shlex.split("nc -l localhost 9966")
 if len(sys.argv) > 1:
 	cmd = sys.argv[1:]
-p = sub.Popen(cmd, stdout=sub.PIPE)
+p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
 
-while True:
-	#select.select([p.stdout.fileno()], [], [])
-	ch = p.stdout.read(1)
-	if len(ch) == 0:
-		break
-	sys.stdout.write(ch)
-	sys.stdout.flush()
-	
+l_r = [p.stdout.fileno(), p.stderr.fileno()]
+while len(l_r) > 0:
+	r,w,x = select.select(l_r, [], [])
+	for i in r:
+		if i is p.stdout.fileno():
+			ch = p.stdout.read(1)
+			if len(ch) == 0:
+				l_r.remove(i)
+			else:
+				sys.stdout.write(ch)
+				sys.stdout.flush()
+		elif i is p.stderr.fileno():
+			ch = p.stderr.read(1)
+			if len(ch) == 0:
+				l_r.remove(i)
+			else:
+				sys.stderr.write(ch)
+				sys.stderr.flush()
+
