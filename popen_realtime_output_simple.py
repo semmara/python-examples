3924299abe7feef5924c7c9332a44ddf8f2b7b38
semmara <semmara@users.noreply.github.com>
2014-03-27 18:23:56 +0100

    Update popen_realtime_output_simple.py

--- popen_realtime_output_simple.py
+++ popen_realtime_output_simple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import subprocess as sub
 
-p = sub.Popen(['python', 'stdout_dummy.py'], stdout=sub.PIPE)
+p = sub.Popen(['cat', 'popen_realtime_output_simple.py'], stdout=sub.PIPE)
 while True:
     line = p.stdout.readline()
     if line != '':
