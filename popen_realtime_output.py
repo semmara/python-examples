3bab967ebb2ebc21f76971c80d1955c52f35f61b
semmara <semmara@users.noreply.github.com>
2014-03-27 20:27:49 +0100

    update

--- popen_realtime_output.py
+++ popen_realtime_output.py
@@ -4,30 +4,25 @@
 
 import subprocess as sub
 import sys
-import select
 import shlex
+import thread
 
 cmd = shlex.split("nc -l localhost 9966")
 if len(sys.argv) > 1:
 	cmd = sys.argv[1:]
 p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
 
-l_r = [p.stdout.fileno(), p.stderr.fileno()]
-while len(l_r) > 0:
-	r,w,x = select.select(l_r, [], [])
-	for i in r:
-		if i is p.stdout.fileno():
-			ch = p.stdout.read(1)
-			if len(ch) == 0:
-				l_r.remove(i)
-			else:
-				sys.stdout.write(ch)
-				sys.stdout.flush()
-		elif i is p.stderr.fileno():
-			ch = p.stderr.read(1)
-			if len(ch) == 0:
-				l_r.remove(i)
-			else:
-				sys.stderr.write(ch)
-				sys.stderr.flush()
+stdout_lock = thread.allocate_lock()
+def t_read(stream):
+	while True:
+		ch = stream.read(1)
+		if len(ch) == 0:
+			return
+		with stdout_lock:
+			sys.stdout.write(ch)
+			sys.stdout.flush()
+
+thread.start_new_thread(t_read, (p.stdout,))
+thread.start_new_thread(t_read, (p.stderr,))
+p.wait()
 
