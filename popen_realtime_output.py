735fd9b1fc5993fe29dd9b84919d78c6b8ecb1c1
semmara <semmara@users.noreply.github.com>
2014-03-29 22:29:11 +0100

    Update popen_realtime_output.py

--- popen_realtime_output.py
+++ popen_realtime_output.py
@@ -6,6 +6,7 @@ import subprocess as sub
 import sys
 import shlex
 import thread
+import select
 
 cmd = shlex.split("nc -l localhost 9966")
 if len(sys.argv) > 1:
@@ -15,12 +16,15 @@ p = sub.Popen(cmd, stdout=sub.PIPE, stderr=sub.PIPE)
 stdout_lock = thread.allocate_lock()
 def t_read(stream):
 	while True:
-		ch = stream.read(1)
-		if len(ch) == 0:
-			return
-		with stdout_lock:
-			sys.stdout.write(ch)
-			sys.stdout.flush()
+		r, _, _ = select.select([stream.fileno()], [], [])
+                for elem in r:
+                        if elem == stream.fileno():
+				ch = stream.read(1)
+				if len(ch) == 0:
+					return
+				with stdout_lock:
+					sys.stdout.write(ch)
+					sys.stdout.flush()
 
 thread.start_new_thread(t_read, (p.stdout,))
 thread.start_new_thread(t_read, (p.stderr,))
