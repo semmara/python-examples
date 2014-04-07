3da45b4b2cc04eb8411e58b738e74ad95f142e65
semmara <semmara@users.noreply.github.com>
2014-04-07 13:27:38 +0200

    update

--- python_binding/run.sh
+++ python_binding/run.sh
@@ -3,7 +3,5 @@
 set -x
 
 cd build && rm -rf * && cmake .. && make VERBOSE=1 && otool -L libmensch.so
-#python -c "import libmensch"
-#install_name_tool -change libboost_python.dylib /prod/boost/1.48.0.4/lib/libboost_python.dylib libmensch.so && otool -L libmensch.so
 python -c "import libmensch"
 
