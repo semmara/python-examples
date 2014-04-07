3c5998bae4aae3220ed953c927504d58e6bb81ec
semmara <semmara@users.noreply.github.com>
2014-04-07 13:20:56 +0200

    fixed linker bug

--- python_binding/run.sh
+++ python_binding/run.sh
@@ -4,6 +4,6 @@ set -x
 
 cd build && rm -rf * && cmake .. && make VERBOSE=1 && otool -L libmensch.so
 #python -c "import libmensch"
-install_name_tool -change libboost_python.dylib /prod/boost/1.48.0.4/lib/libboost_python.dylib libmensch.so && otool -L libmensch.so
+#install_name_tool -change libboost_python.dylib /prod/boost/1.48.0.4/lib/libboost_python.dylib libmensch.so && otool -L libmensch.so
 python -c "import libmensch"
 
